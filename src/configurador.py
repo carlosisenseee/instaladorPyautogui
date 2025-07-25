02b8dc93fc83ae5550531f1516ae93432ed83a7b
Carlos Notebook <carlosalfredoisensee@gmail.com>
2025-07-25 10:22:25 -0300

    aprimorando projeto

--- src/configurador.py
+++ src/configurador.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 mensagem = r'\\fs01\informatica$\PROGRAMA ADVOGADOS'
 
 def configuraNavegador(caminho):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     pyautogui.press('win')
     time.sleep(1)
     pyautogui.write('Chrome')
@@ -15,7 +16,7 @@ def configuraNavegador(caminho):
 
     while True:
         try:
-            if pyautogui.locateOnScreen('google.png', confidence=0.9):
+            if pyautogui.locateOnScreen(os.path.join(BASE_DIR, 'Assets', 'google.png'), confidence=0.9):
                 print('Found')
                 break
         except:
@@ -28,7 +29,7 @@ def configuraNavegador(caminho):
     pyautogui.press('enter')
     pyautogui.sleep(1)
 
-    local = pyautogui.locateOnScreen('arquivo_html_1.png', confidence=0.5)
+    local = pyautogui.locateOnScreen(os.path.join(BASE_DIR, 'Assets', 'arquivo_html_1.png'), confidence=0.5)
     if local:
         centro = pyautogui.center(local)
         pyautogui.moveTo(centro)
@@ -39,14 +40,7 @@ def configuraNavegador(caminho):
     pyautogui.press('down')
     pyautogui.press('enter')
 
-    # local2 = pyautogui.locateOnScreen('escolher_arquivo.png', confidence=0.5)
-    # if local2:
-    #     centro = pyautogui.center(local2)
-    #     pyautogui.moveTo(centro)
-
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
+    pyautogui.press('tab', 3)
     pyautogui.press('enter')
 
     pyautogui.sleep(1)
@@ -55,6 +49,42 @@ def configuraNavegador(caminho):
 
     pyautogui.sleep(0.5)
     pyautogui.press('enter')
+
+    while True:
+        try:
+            if pyautogui.locateOnScreen(os.path.join(BASE_DIR, 'Assets', 'ok.png'), confidence=0.9):
+                print('Found')
+                break
+        except:
+            pyautogui.sleep(0.5)
+            print('Not Found')
+
+    pyautogui.sleep(0.5)
+    pyautogui.press('enter')
+
+    pyautogui.sleep(0.5)
+    pyautogui.hotkey('ctrl', 'l')
+    pyautogui.sleep(0.2)
+    pyautogui.write('chrome://settings/defaultBrowser')
+    # dar um jeito
+
+
+    pyautogui.hotkey('ctrl', 't')
+    pyautogui.sleep(0.4)
+    pyautogui.write('CAMINHO OUTLOOK')
+
+    pyautogui.hotkey('ctrl', 't')
+    pyautogui.sleep(0.4)
+    pyautogui.write('CAMINHO INTRANET')
+
+
+    pyautogui.write('chrome://settings/onStartup')
+    pyautogui.sleep(0.5)
+    pyautogui.press('tab')
+    pyautogui.press('down', 2)
+    pyautogui.press('tab', 2)
+    pyautogui
+
 
     # pyautogui.write(r'C:\Users\Carlos\Downloads')
     # pyautogui.press('enter')
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import time
 import tkinter as tk
 from tkinter import messagebox
 
-import main2
+import configurador
 
 # CNTL + L puxa a barra de pesquisa
 
@@ -16,51 +16,27 @@ def mostrar_opcao():
     if opcao_escolhida == r'\Juridico Juliano': # COLOCAR NOME DO ARQUIVO
         caminho = caminho_padrao + opcao_escolhida
         print(caminho)
-        main2.configuraNavegador(caminho=caminho)
+        configurador.configuraNavegador(caminho=caminho)
 
     login = entrada_nome.get()
     messagebox.showinfo("Opção Selecionada", f"Você escolheu: {opcao_escolhida} \n Login: {login}")
 
-# Criar janela
 janela = tk.Tk()
 janela.title("Menu com RadioButton")
 janela.geometry("960x540")
 
-# Variável para armazenar a opção selecionada
 opcao = tk.StringVar()
 opcao.set(r"\Juridico Juliano")  # Valor inicial
 
-# Campo Nome Completo
-# tk.Label(janela, text="Nome Completo:").pack(pady=10, anchor='w', padx=10)
-# entrada_nome = tk.Entry(janela, width=50)
-# entrada_nome.pack(anchor='w', padx=10)
-#
-# Campos login
-# tk.Label(janela, text="Login: ").pack(pady=10, anchor='w', padx=10)
-# entrada_nome = tk.Entry(janela, width=50)
-# entrada_nome.pack(anchor='w', padx=10)
-
-# Radio buttons
 tk.Label(janela, text="Pasta para navegador:").pack(pady=10, anchor='w', padx=10)
 tk.Radiobutton(janela, text="Juridico Juliano", variable=opcao, value=r"\Juridico Juliano").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Juridico Tatiane", variable=opcao, value="Opção 2").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Juridico Cintia", variable=opcao, value="Opção 3").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 4", variable=opcao, value="Opção 4").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 5", variable=opcao, value="Opção 5").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 6", variable=opcao, value="Opção 6").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 7", variable=opcao, value="Opção 7").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 8", variable=opcao, value="Opção 8").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 9", variable=opcao, value="Opção 9").pack(anchor="w", padx=10)
-# tk.Radiobutton(janela, text="Opcao 10", variable=opcao, value="Opção 10").pack(anchor="w", padx=10)
+tk.Radiobutton(janela, text="Juridico Tatiane", variable=opcao, value=r"\Juridico Tatiane").pack(anchor="w", padx=10)
+tk.Radiobutton(janela, text="Juridico Cintia", variable=opcao, value=r"\Juridico Cintia").pack(anchor="w", padx=10)
 
-# Botão para mostrar a escolha
 btn_mostrar = tk.Button(janela, text="Confirmar", command=mostrar_opcao)
 btn_mostrar.pack(pady=10)
 
-# Rodar interface
 janela.mainloop()
-
-main2.configuraNavegador(caminho=mostrar_opcao())
 
 # mensagem = r'\\fs01\informatica$\PROGRAMA ADVOGADOS'
 #
